fca1f399412d30f786477e91860d1f779dc0515b
Barış Özgen <barisozgen@hotmail.com>
2023-04-16 08:57:52 +0400

    home_page desining

--- photo_ml/lib/home_page.dart
+++ photo_ml/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photo_ml/theme/pallette.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PhotoAI'),
+        leading: const Icon(Icons.menu),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+// header image Stack
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 129,
+                  width: 129,
+                  margin: const EdgeInsets.only(top: 7),
+                  decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle),
+                ),
+              ),
+              Center(
+                child: Container(
+                    height: 132,
+                    width: 132,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/virtualAssistant.png')))),
+              )
+            ],
+          ),
+// chat buble Container
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 7),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 29,
+            ).copyWith(top: 14),
+            decoration:
+                BoxDecoration(border: Border.all(color: Pallete.borderColor)),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 9.0),
+              child: Text(
+                'Your Personal Assistant - What Can I Do for You Today?',
+                style: TextStyle(
+                    fontFamily: 'Cera Pro',
+                    color: Pallete.mainFontColor,
+                    fontSize: 24),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
