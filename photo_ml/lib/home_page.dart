c941921e5ea30b2b3e34d9adabd0fe3b5c16ccb7
Barış Özgen <barisozgen@hotmail.com>
2023-04-17 21:30:49 +0400

    speech to text init

--- photo_ml/lib/home_page.dart
+++ photo_ml/lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:photo_ml/feature_list_item.dart';
 import 'package:photo_ml/theme/pallette.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,14 +11,50 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-
 class _HomePageState extends State<HomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
 
   @override
   void initState() {
     super.initState();
-    
+    initSpeechToText();
   }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+// Each time to start a speech recognition session
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,116 +69,127 @@ class _HomePageState extends State<HomePage> {
                 image: AssetImage('assets/images/bakcgroundSeamless.png'),
                 fit: BoxFit.cover,
                 opacity: 0.5)),
-        child: Column(
-          children: [
-            // header image Stack
-            Stack(
-              children: [
-                Center(
-                  child: Container(
-                    height: 129,
-                    width: 129,
-                    margin: const EdgeInsets.only(top: 7),
-                    decoration: const BoxDecoration(
-                        color: Pallete.firstAssistantCircleColor,
-                        shape: BoxShape.circle),
-                  ),
-                ),
-                Center(
-                  child: Container(
-                      height: 158,
-                      width: 158,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // header image Stack
+              Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 129,
+                      width: 129,
+                      margin: const EdgeInsets.only(top: 7),
                       decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                              image: AssetImage(
-                                  'assets/images/aiAssistant.png')))),
-                )
-              ],
-            ),
-            // chat buble Container
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 7),
-              margin: const EdgeInsets.symmetric(
-                horizontal: 29,
-              ).copyWith(top: 14),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Pallete.borderColor),
-                  borderRadius:
-                      BorderRadius.circular(14).copyWith(topLeft: Radius.zero)),
-              child: Column(
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.only(left: 7, top: 14, bottom: 0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'What can I do for you today?',
-                        style: TextStyle(
-                            fontFamily: 'Cera Pro',
-                            color: Pallete.mainFontColor,
-                            fontSize: 14),
-                      ),
+                          color: Pallete.firstAssistantCircleColor,
+                          shape: BoxShape.circle),
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 7, bottom: 14, top: 0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Your Personal AI Assistant',
-                        style: TextStyle(
-                            fontFamily: 'Cera Pro',
-                            color: Pallete.mainFontColor,
-                            fontSize: 24),
-                      ),
-                    ),
-                  ),
+                  Center(
+                    child: Container(
+                        height: 158,
+                        width: 158,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/aiAssistant.png')))),
+                  )
                 ],
               ),
-            ),
-            // features list
-            Container(
-              padding: const EdgeInsets.all(14),
-              alignment: Alignment.centerLeft,
-              margin: const EdgeInsets.only(top: 14.0, left: 14, right: 14),
-              child: const Text(
-                'Discover Your Possibilities with Our Features!',
-                style: TextStyle(
-                    fontFamily: 'Cera Pro',
-                    color: Pallete.mainFontColor,
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold),
+              // chat buble Container
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 14, vertical: 7),
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 29,
+                ).copyWith(top: 14),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Pallete.borderColor),
+                    borderRadius: BorderRadius.circular(14)
+                        .copyWith(topLeft: Radius.zero)),
+                child: Column(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(left: 7, top: 14, bottom: 0),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'What can I do for you today?',
+                          style: TextStyle(
+                              fontFamily: 'Cera Pro',
+                              color: Pallete.mainFontColor,
+                              fontSize: 14),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 7, bottom: 14, top: 0),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Your Personal AI Assistant',
+                          style: TextStyle(
+                              fontFamily: 'Cera Pro',
+                              color: Pallete.mainFontColor,
+                              fontSize: 24),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Column(
-              children: const [
-                FeatureListItem(
-                  backgroundColor: Pallete.firstListItemColor,
-                  titleText: 'ChatGPT Text',
-                  descriptionText:
-                      'Unlock your potential with ChatGPT: The smarter way to stay organized and informed.',
+              // features list
+              Container(
+                padding: const EdgeInsets.all(14),
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.only(top: 14.0, left: 14, right: 14),
+                child: const Text(
+                  'Discover Your Possibilities with Our Features!',
+                  style: TextStyle(
+                      fontFamily: 'Cera Pro',
+                      color: Pallete.mainFontColor,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold),
                 ),
-                FeatureListItem(
-                  backgroundColor: Pallete.secondListItemColor,
-                  titleText: 'Dall-E Image',
-                  descriptionText:
-                      'Unleash your creativity with Dall-E\'s personal assistant: Inspire and create effortlessly.',
-                ),
-                FeatureListItem(
-                  backgroundColor: Pallete.thirdListItemColor,
-                  titleText: 'Future of Voice Assistance',
-                  descriptionText:
-                      'Experience the perfect blend of intelligence and imagination with Dall-E and ChatGPT.',
-                )
-              ],
-            )
-          ],
+              ),
+              Column(
+                children: const [
+                  FeatureListItem(
+                    backgroundColor: Pallete.firstListItemColor,
+                    titleText: 'ChatGPT Text',
+                    descriptionText:
+                        'Unlock your potential with ChatGPT: The smarter way to stay organized and informed.',
+                  ),
+                  FeatureListItem(
+                    backgroundColor: Pallete.secondListItemColor,
+                    titleText: 'Dall-E Image',
+                    descriptionText:
+                        'Unleash your creativity with Dall-E\'s personal assistant: Inspire and create effortlessly.',
+                  ),
+                  FeatureListItem(
+                    backgroundColor: Pallete.thirdListItemColor,
+                    titleText: 'Future of Voice Assistance',
+                    descriptionText:
+                        'Experience the perfect blend of intelligence and imagination with Dall-E and ChatGPT.',
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
 // mic button
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          } else if (speechToText.isListening) {
+            await stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
         backgroundColor: Pallete.secondAssistantCircleColor,
         child: const Icon(Icons.mic, color: Color.fromARGB(255, 255, 255, 255)),
       ),
