f4fa3fc132a619be7492e8e0263ac7e0621f13a6
Barış Özgen <barisozgen@hotmail.com>
2023-04-23 08:15:50 +0400

    some design changes

--- photo_ml/lib/message_baloon.dart
+++ photo_ml/lib/message_baloon.dart
@@ -18,72 +18,80 @@ class MessageBaloon extends StatelessWidget {
   Widget build(BuildContext context) {
     return FadeInRight(
       child: Visibility(
-        child: Container(
-          constraints: const BoxConstraints(minWidth: 129, maxWidth: 129),
-          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 7),
-          margin: const EdgeInsets.only(bottom: 7),
+        child: Align(
           alignment: isAI ? Alignment.centerLeft : Alignment.centerRight,
-          decoration: BoxDecoration(
-              border: Border.all(color: Pallete.borderColor),
-              color: isAI
-                  ? const Color.fromARGB(255, 255, 255, 255)
-                  : const Color.fromARGB(255, 233, 233, 233),
-              borderRadius: BorderRadius.circular(14).copyWith(
-                  bottomLeft: isAI ? Radius.zero : const Radius.circular(14),
-                  bottomRight: !isAI ? Radius.zero : const Radius.circular(14),
-                  topRight: !isAI
-                      ? const Radius.circular(29)
-                      : const Radius.circular(14),
-                  topLeft: isAI
-                      ? const Radius.circular(29)
-                      : const Radius.circular(14))),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 7,
-                  top: 7,
-                  bottom: 0,
-                ),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    isAI ? 'AI' : 'You',
-                    style: const TextStyle(
-                        fontFamily: 'Cera Pro',
-                        color: Pallete.mainFontColor,
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold),
+          child: Flexible(
+            child: Container(
+              width: text.length < 29 ? 229 : double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 7),
+              margin: const EdgeInsets.only(bottom: 7)
+                  .copyWith(left: isAI ? 0 : 29, right: isAI ? 29 : 0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Pallete.borderColor),
+                  color: isAI
+                      ? const Color.fromARGB(255, 255, 255, 255)
+                      : const Color.fromARGB(255, 233, 233, 233),
+                  borderRadius: BorderRadius.circular(14).copyWith(
+                      bottomLeft:
+                          isAI ? Radius.zero : const Radius.circular(14),
+                      bottomRight:
+                          !isAI ? Radius.zero : const Radius.circular(14),
+                      topRight: !isAI
+                          ? const Radius.circular(29)
+                          : const Radius.circular(14),
+                      topLeft: isAI
+                          ? const Radius.circular(29)
+                          : const Radius.circular(14))),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 7,
+                      top: 7,
+                      bottom: 0,
+                    ),
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        isAI ? 'AI' : 'You',
+                        style: const TextStyle(
+                            fontFamily: 'Cera Pro',
+                            color: Pallete.mainFontColor,
+                            fontSize: 12,
+                            backgroundColor: Pallete.secondAssistantCircleColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 7,
-                  top: 7,
-                  bottom: 7,
-                ),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    text,
-                    style: const TextStyle(
-                        fontFamily: 'Cera Pro',
-                        color: Pallete.mainFontColor,
-                        fontSize: 15),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 7,
+                      top: 7,
+                      bottom: 7,
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        text,
+                        style: const TextStyle(
+                            fontFamily: 'Cera Pro',
+                            color: Pallete.mainFontColor,
+                            fontSize: 15),
+                      ),
+                    ),
                   ),
-                ),
-              ),
 
-              // image generated
-              if (imageUrl != null)
-                Padding(
-                  padding: const EdgeInsets.all(7.0),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(4),
-                      child: Image.network(imageUrl!)),
-                ),
-            ],
+                  // image generated
+                  if (imageUrl != null)
+                    Padding(
+                      padding: const EdgeInsets.all(7.0),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(imageUrl!)),
+                    ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
