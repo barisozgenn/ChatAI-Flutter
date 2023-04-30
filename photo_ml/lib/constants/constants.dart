3b18f20da2899abef38f226b3fac0c4b7b94804f
Barış Özgen <barisozgen@hotmail.com>
2023-04-30 17:16:46 +0400

    here_your_api_key

--- photo_ml/lib/constants/constants.dart
+++ photo_ml/lib/constants/constants.dart
@@ -1,3 +1,3 @@
-const openAIAPIKey = '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';
+const openAIAPIKey = 'here_your_api_key';
 const openAIAPIURL = 'https://api.openai.com/v1/chat/completions';
 const openAIImageAPIURL = 'https://api.openai.com/v1/images/generations';
